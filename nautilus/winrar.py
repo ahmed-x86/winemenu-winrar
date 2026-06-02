9dafddcd5acd33d6e0070f5eeddfb3c391fdcbdd
ahmed-x86 <ahmdansmr7@gmail.com>
2026-06-02 13:36:55 +0300

    .

--- nautilus/winrar.py
+++ nautilus/winrar.py
@@ -15,7 +15,7 @@ class WinRARMenuProvider(GObject.GObject, Nautilus.MenuProvider):
         super().__init__()
         self.valid_exts = {'.001', '.7z', '.arj', '.bz', '.bz2', '.cab', '.gz', '.iso', '.jar', '.lha', '.lz', '.lzh', '.rar', '.tar', '.taz', '.tbz', '.tbz2', '.tgz', '.tlz', '.txz', '.tzst', '.uu', '.uue', '.xxe', '.xz', '.z', '.zip', '.zipx', '.zst'}
         
-        self.actions = [
+        self.extract_actions = [
             {'name': 'Extract Here', 'id': 'EXTRACT_HERE'}, 
             {'name': 'Extract to...', 'id': 'EXTRACT_TO'}, 
             {'name': 'Extract files...', 'id': 'EXTRACT_DIALOG'}, 
@@ -27,40 +27,51 @@ class WinRARMenuProvider(GObject.GObject, Nautilus.MenuProvider):
         if not files:
             return []
 
+        has_archives = True
         for file in files:
             if file.is_directory():
-                return []
+                has_archives = False
+                break
             
             filename = file.get_name()
             ext = os.path.splitext(filename)[1].lower()
             
             if '*' not in self.valid_exts and ext not in self.valid_exts and not re.search(r'\.(r\d+|z\d+|part\d+\.rar)$', filename.lower()):
-                return []
+                has_archives = False
+                break
 
         main_item = Nautilus.MenuItem(name='WineMenu::WinRAR', label='WinRAR')
         main_submenu = Nautilus.Menu()
         main_item.set_submenu(main_submenu)
 
-        for idx, action in enumerate(self.actions):
+        first_name = files[0].get_name()
+        compress_actions = [
+            {'name': f'Add to "{first_name}.rar"', 'id': 'COMPRESS_QUICK'},
+            {'name': 'Add to archive...', 'id': 'COMPRESS_DIALOG'}
+        ]
+
+        for action in compress_actions:
             item = Nautilus.MenuItem(name=f"WinRARAction::{action['id']}", label=action['name'])
             item.connect('activate', self.execute_wine_app, files, action['id'])
             main_submenu.append_item(item)
+
+        if has_archives:
+            for action in self.extract_actions:
+                item = Nautilus.MenuItem(name=f"WinRARAction::{action['id']}", label=action['name'])
+                item.connect('activate', self.execute_wine_app, files, action['id'])
+                main_submenu.append_item(item)
 
         return [main_item]
 
     def get_base_archive_name(self, filename):
         name = re.sub(r'\.part\d+\.rar$', '.rar', filename, flags=re.IGNORECASE)
         if name != filename: return name
-        
         name = re.sub(r'\.\d{3}$', '', filename)
         if name != filename: return name
-        
         name = re.sub(r'\.r\d+$', '.rar', filename, flags=re.IGNORECASE)
         if name != filename: return name
-        
         name = re.sub(r'\.z\d+$', '.zip', filename, flags=re.IGNORECASE)
         if name != filename: return name
-
         return filename
 
     def sort_files_priority(self, file):
@@ -71,8 +82,50 @@ class WinRARMenuProvider(GObject.GObject, Nautilus.MenuProvider):
             return 1
         return 2
 
-    # التنفيذ المباشر (مثل الكود القديم) بدون Threading
     def execute_wine_app(self, menu, files, action_id):
+        winrar_exe = os.path.expanduser('~/.wine/drive_c/Program Files/WinRAR/WinRAR.exe')
+
+
+        if action_id.startswith('COMPRESS'):
+            first_file = files[0]
+            working_dir = os.path.dirname(first_file.get_location().get_path())
+            first_name = first_file.get_name()
+            
+            if action_id == 'COMPRESS_QUICK':
+                archive_name = first_name + '.rar'
+                archive_linux_path = os.path.join(working_dir, archive_name)
+            elif action_id == 'COMPRESS_DIALOG':
+                try:
+                    archive_linux_path = subprocess.check_output(
+                        ['zenity', '--file-selection', '--save', '--confirm-overwrite', f'--filename={os.path.join(working_dir, first_name + ".rar")}', '--title=Save Archive As...'],
+                        stderr=subprocess.DEVNULL
+                    ).decode('utf-8').strip()
+                    if not archive_linux_path:
+                        return
+                except (subprocess.CalledProcessError, FileNotFoundError):
+                    return
+
+            try:
+                archive_win_path = subprocess.check_output(
+                    ['winepath', '-w', archive_linux_path],
+                    stderr=subprocess.DEVNULL
+                ).decode('utf-8').strip()
+            except subprocess.CalledProcessError:
+                archive_win_path = archive_linux_path
+
+            final_cmd = ['wine', winrar_exe, 'a', archive_win_path]
+
+            for file in files:
+                final_cmd.append(file.get_name())
+
+            subprocess.Popen(
+                final_cmd, 
+                cwd=working_dir,
+                stdout=subprocess.DEVNULL,
+                stderr=subprocess.DEVNULL
+            )
+            return
+
         processed_bases = set()
         files_to_process = []
         
@@ -118,8 +171,6 @@ class WinRARMenuProvider(GObject.GObject, Nautilus.MenuProvider):
                 win_filepath = linux_filepath
 
             working_dir = os.path.dirname(linux_filepath)
-            winrar_exe = os.path.expanduser('~/.wine/drive_c/Program Files/WinRAR/WinRAR.exe')
-            
             final_cmd = ['wine', winrar_exe]
             
             if action_id == 'EXTRACT_HERE':
