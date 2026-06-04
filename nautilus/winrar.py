5f35628bb3671b03cbdd1460dfef34b2ae9cf0e6
ahmed-x86 <ahmdansmr7@gmail.com>
2026-06-04 12:45:34 +0300

    1

--- nautilus/winrar.py
+++ nautilus/winrar.py
@@ -85,38 +85,96 @@ class WinRARMenuProvider(GObject.GObject, Nautilus.MenuProvider):
     def execute_wine_app(self, menu, files, action_id):
         winrar_exe = os.path.expanduser('~/.wine/drive_c/Program Files/WinRAR/WinRAR.exe')
 
-
         if action_id.startswith('COMPRESS'):
-            first_file = files[0]
-            working_dir = os.path.dirname(first_file.get_location().get_path())
-            first_name = first_file.get_name()
+            working_dir = os.path.dirname(files[0].get_location().get_path())
+            first_name = files[0].get_name()
             
+            files_to_compress = []
+            for file in files:
+                linux_path = file.get_location().get_path()
+                try:
+                    win_path = subprocess.check_output(['winepath', '-w', linux_path], stderr=subprocess.DEVNULL).decode('utf-8').strip()
+                    files_to_compress.append(win_path)
+                except subprocess.CalledProcessError:
+                    files_to_compress.append(linux_path)
+
             if action_id == 'COMPRESS_QUICK':
                 archive_name = first_name + '.rar'
-                archive_linux_path = os.path.join(working_dir, archive_name)
+                win_archive_path = os.path.join(os.path.dirname(files_to_compress[0]), archive_name)
+                final_cmd = ['wine', winrar_exe, 'a', '-ep1', win_archive_path] + files_to_compress
+            
             elif action_id == 'COMPRESS_DIALOG':
                 try:
-                    archive_linux_path = subprocess.check_output(
-                        ['zenity', '--file-selection', '--save', '--confirm-overwrite', f'--filename={os.path.join(working_dir, first_name + ".rar")}', '--title=Save Archive As...'],
-                        stderr=subprocess.DEVNULL
-                    ).decode('utf-8').strip()
-                    if not archive_linux_path:
+                    yad_cmd = [
+                        'yad', '--title=WinRAR Settings',
+                        '--form', '--width=500', '--center',
+                        '--text=<b>Configure Archive Settings:</b>',
+                        '--separator=|||',
+                        '--field=Archive Name', first_name,
+                        '--field=Save Location:DIR', working_dir,
+                        '--field=Archive Type:CB', '^rar!zip',
+                        '--field=Password:H', '',
+                        '--field=Split Size (e.g., 100M, 1G)', '',
+                        '--field=Compression Level:CB', '0 (Store)!1 (Fastest)!2 (Fast)!^3 (Normal)!4 (Good)!5 (Best)',
+                        '--field=Delete original files after archiving:CHK', 'FALSE',
+                        '--field=Create Solid Archive:CHK', 'FALSE',
+                        '--field=Dictionary Size (e.g., 32m, 128m)', ''
+                    ]
+                    
+                    result = subprocess.check_output(yad_cmd, stderr=subprocess.DEVNULL).decode('utf-8').strip()
+                    if not result:
                         return
-                except (subprocess.CalledProcessError, FileNotFoundError):
+                    
+                    parts = result.split('|||')
+                    archive_base_name = parts[0].strip()
+                    save_location = parts[1].strip()
+                    archive_type = parts[2].strip()
+                    password = parts[3].strip()
+                    split_size = parts[4].strip()
+                    
+                    comp_level_raw = parts[5].strip()
+                    comp_level = ""
+                    if comp_level_raw:
+                        match = re.search(r'\d', comp_level_raw)
+                        if match:
+                            comp_level = match.group()
+                    
+                    del_files = parts[6].strip()
+                    solid_arch = parts[7].strip()
+                    dict_size = parts[8].strip()
+
+                    if not archive_base_name.lower().endswith(f'.{archive_type}'):
+                        archive_name = f"{archive_base_name}.{archive_type}"
+                    else:
+                        archive_name = archive_base_name
+
+                    archive_linux_path = os.path.join(save_location, archive_name)
+                    
+                    try:
+                        archive_win_path = subprocess.check_output(['winepath', '-w', archive_linux_path], stderr=subprocess.DEVNULL).decode('utf-8').strip()
+                    except subprocess.CalledProcessError:
+                        archive_win_path = archive_linux_path
+
+                    final_cmd = ['wine', winrar_exe, 'a']
+                    
+                    if password:
+                        final_cmd.append(f'-hp{password}')
+                    if split_size:
+                        final_cmd.append(f'-v{split_size}')
+                    if comp_level in ['0', '1', '2', '3', '4', '5']:
+                        final_cmd.append(f'-m{comp_level}')
+                    if del_files == 'TRUE':
+                        final_cmd.append('-df')
+                    if solid_arch == 'TRUE':
+                        final_cmd.append('-s')
+                    if dict_size:
+                        final_cmd.append(f'-md{dict_size}')
+                    
+                    final_cmd.append(archive_win_path)
+                    final_cmd.extend(files_to_compress)
+
+                except subprocess.CalledProcessError:
                     return
-
-            try:
-                archive_win_path = subprocess.check_output(
-                    ['winepath', '-w', archive_linux_path],
-                    stderr=subprocess.DEVNULL
-                ).decode('utf-8').strip()
-            except subprocess.CalledProcessError:
-                archive_win_path = archive_linux_path
-
-            final_cmd = ['wine', winrar_exe, 'a', archive_win_path]
-
-            for file in files:
-                final_cmd.append(file.get_name())
 
             subprocess.Popen(
                 final_cmd, 
@@ -143,7 +201,7 @@ class WinRARMenuProvider(GObject.GObject, Nautilus.MenuProvider):
         if action_id == 'EXTRACT_DIALOG':
             try:
                 dest_linux_path = subprocess.check_output(
-                    ['zenity', '--file-selection', '--directory', '--title=Select Extraction Destination'],
+                    ['yad', '--file-selection', '--directory', '--title=Select Extraction Destination'],
                     stderr=subprocess.DEVNULL
                 ).decode('utf-8').strip()
                 
